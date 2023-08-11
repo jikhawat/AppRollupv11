291bcaa1b42f31e3f3582a9372350bc4400e2d03
uxui <adverorga@gmail.com>
2023-08-11 08:53:00 +0700

    first commit

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Volumes/Works/WorkApp/rollup/rollupapp/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
+  /Volumes/Works/WorkApp/rollup/rollupapp/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m
---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Volumes/Works/WorkApp/rollup/rollupapp/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
+  /Volumes/Works/WorkApp/rollup/rollupapp/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
